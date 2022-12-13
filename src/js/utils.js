2c7bda7b5638546874aa4d93d5fe720add5b1cc5
wesleydamasceno <wesleydamasceno.co@gmail.com>
2022-12-13 14:14:50 -0300

    feat: create method to updateStatus

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,13 @@
 export const status = document.querySelector('.status');
 export const showStatus = () => status.showModal();
 export const closeStatus = () => status.close();
+
+HTMLElement.prototype.updateStatus = function (firstClass, secondClass, removeAllClass = false) {
+  if (removeAllClass === true) {
+    this.classList.remove(firstClass);
+    this.classList.remove(secondClass);
+  } else {
+    this.classList.add(firstClass);
+    this.classList.remove(secondClass);
+  }
+}
